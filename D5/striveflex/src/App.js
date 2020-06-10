6a248ba41713b0093c50202207a7b4bd8b71c994
Hammad-87 <pakswim@gmail.com>
2020-06-10 10:08:45 +0200

    D5

---
+++ D5/striveflex/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarflex from './component/Navbarflex'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarflex/>
+    </div>
+  );
+}
+
+export default App;
