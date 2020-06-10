19b6e732128f08e0ce9ca817eb91e5db06cf5e59
Hammad-87 <pakswim@gmail.com>
2020-06-10 10:27:36 +0200

    footer & CSS

--- D5/striveflex/src/App.js
+++ D5/striveflex/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbarflex from './component/Navbarflex'
+import Footerflex from './component/Footerflex'
 
 function App() {
   return (
     <div className="App">
       <Navbarflex/>
+      <Footerflex/>
+
     </div>
   );
 }
--- D5/striveflex/src/component/Navbarflex.jsx
+++ D5/striveflex/src/component/Navbarflex.jsx
@@ -4,7 +4,7 @@ import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 export default class Navbarflex extends Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
+            <Navbar variant="dark" expand="lg" style={{ backgroundColor: "#221f1f" }}>
                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
