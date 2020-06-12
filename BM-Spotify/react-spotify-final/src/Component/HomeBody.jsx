4f88d0ed9ae3825ef387e436d45e3108f89808f1
Hammad-87 <pakswim@gmail.com>
2020-06-12 14:19:31 +0200

    Body

---
+++ BM-Spotify/react-spotify-final/src/Component/HomeBody.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const HomeBody = () => (
+        
+        <>
+            
+                <div className="row">
+                    <div className="col-9 col-lg-11 mainLinks d-none d-md-flex">
+                        <a href="#">TRENDING</a>
+                        <a href="#">PODCAST</a>
+                        <a href="#">MOODS AND GENRES</a>
+                        <a href="#">NEW RELEASES</a>
+                        <a href="#">DISCOVER</a>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-10"></div>
+                </div>
+
+                <div className="row">
+                    <div className="col-10">
+                    <div id="rock">
+                        <h2>Eminem</h2>
+                        <div
+                        className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                        id="Eminem"
+                        ></div>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-10">
+                    <div id="rock">
+                        <h2>Metallica</h2>
+                        <div
+                        className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                        id="Metallica"
+                        ></div>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                 <div className="col-10">
+                    <div id="rock">
+                        <h2>Behemoth</h2>
+                        <div
+                        className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                        id="Behemoth"
+                        ></div>
+                    </div>
+                 </div>
+                </div>
+        </>
+        
+      
+    
+    
+    )
+
+
+
+export default HomeBody
